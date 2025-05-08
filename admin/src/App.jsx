1b13a742459f5f20dba5bfabd7aa2481cc9d447d
Harish000007 <harishdhami080@gmail.com>
2025-05-08 16:12:30 +0545

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import Login from "./components/Login";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-export const backendUrl = https://grocery-backend-ybjg.onrender.com;
+export const backendUrl = "https://grocery-backend-ybjg.onrender.com";
 export const currency = 'Rs.';
 
 const App = () => {
